90ea50704d05cd039a4a5b17bfddbb320c8f207e
Can <1217910659@qq.com>
2023-09-07 00:24:50 +0800

    count sort

--- sort_algorithm/count_sort.go
+++ sort_algorithm/count_sort.go
@@ -1,1 +1,33 @@
 package sort_algorithm
+
+import "fmt"
+
+type CountSort []int
+
+func (c CountSort) SortImpl() {
+	if len(c) < 2 {
+		return
+	}
+	min, max := c[0], c[0]
+	for _, cc := range c {
+		if cc < min {
+			min = cc
+		}
+		if cc > max {
+			max = cc
+		}
+	}
+	d := max - min
+	arr := make([]int, d+1)
+	for _, cc := range c {
+		arr[cc-min]++
+	}
+	j := 0
+	for i, cc := range arr {
+		for ; cc > 0; cc-- {
+			c[j] = i + min
+			j++
+		}
+	}
+	fmt.Println(c)
+}
--- sort_algorithm/sort_test.go
+++ sort_algorithm/sort_test.go
@@ -10,7 +10,8 @@ func TestSort(t *testing.T) {
 	//sorts := &ShellSort{5, 1, 2, 41, 2, 1, 100, 99, 1, 2}
 	//sorts := &MergeSort{5, 1, 2, 41, 2, 1, 100, 99, 1, 2}
 	//sorts := &QuickSort{5, 1, 2, 41, 2, 1, 100, 99, 1, 2}
-	sorts := &HeapSort{5, 1, 2, 41, 2, 1, 100, 99, 1, 2}
+	//sorts := &HeapSort{5, 1, 2, 41, 2, 1, 100, 99, 1, 2}
+	sorts := &CountSort{5, 1, 2, 41, 2, 1, 100, 99, 1, 2}
 	sort(sorts)
 }
 
